b94f829bb24570782b9f6bbcfdec4b391091b778
Jen-Chieh <jcs090218@gmail.com>
2019-05-17 13:21:12 +0800

    Bump version.

--- project-abbrev.el
+++ project-abbrev.el
@@ -6,7 +6,7 @@
 ;; Author: Shen, Jen-Chieh <jcs090218@gmail.com>
 ;; Description: Customize abbreviation expansion in the project.
 ;; Keyword: abbreviation customizable shortcut
-;; Version: 0.0.1
+;; Version: 0.0.2
 ;; Package-Requires: ((emacs "24.4"))
 ;; URL: https://github.com/jcs090218/project-abbrev
 
